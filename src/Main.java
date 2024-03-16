d7e9a2e01a90e4805e9b300f005732798dbe22bb
dtp712 <92659203+dtp712@users.noreply.github.com>
2024-03-16 14:39:40 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
+        int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
 
+        // tinh tong
         Thread sumThread = new Thread(() -> {
             int sum = 0;
-            for (int number : numbers) {
-                sum += number;
+            for (int num : numbers) {
+                sum += num;
             }
-            System.out.println("Tổng của các phần tử trong mảng: " + sum);
+            System.out.println("Tổng của các phần tử trong mảng là: " + sum);
         });
 
+        // tim phan tu lon nhat
         Thread maxThread = new Thread(() -> {
-            int max = numbers[0];
-            for (int i = 1; i < numbers.length; i++) {
-                if (numbers[i] > max) {
-                    max = numbers[i];
+            int max = Integer.MIN_VALUE;
+            for (int num : numbers) {
+                if (num > max) {
+                    max = num;
                 }
             }
-            System.out.println("Phần tử lớn nhất trong mảng: " + max);
+            System.out.println("Phần tử lớn nhất trong mảng là: " + max);
         });
 
-        sumThread.start();
+        // Khoi dong 2 luong
         maxThread.start();
+        sumThread.start();
     }
 }
